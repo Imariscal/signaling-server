03049c5347cd35ec196e3eb1fda99071632b237b
Ignacio Mariscal <ignacio.mariscal@tateeda.com>
2024-12-24 11:47:41 -0600

    Initial commit: signaling server

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ io.on('connection', (socket) => {
 });
 
 const port = process.env.PORT || 3000;
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log(`Signaling server running on http://localhost:${port}`);
 });
